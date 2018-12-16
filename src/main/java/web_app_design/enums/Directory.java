967d3054cb728c47e498c964b4d1d8aacdcb1abf
asemrin <ahmad.semrin@hotmail.com>
2018-12-16 18:59:41 +0200

    Lit-02.01: Updated XSLT file.

--- src/main/java/web_app_design/enums/Directory.java
+++ src/main/java/web_app_design/enums/Directory.java
@@ -8,7 +8,7 @@ public enum Directory {
     PROJECT_PATH(HOME_PATH.getDirectory() + "/Documents/IdeaProjects/Maven Projects/Literatum 2.0 - Ahmad Semrin"),
     UPLOADED_FILES_PATH(PROJECT_PATH.getDirectory() + "/Uploaded Files"),
     JATS_DTD_PATH(PROJECT_PATH.getDirectory() + "/jats.dtd"),
-    JATS_XSLT_PATH(PROJECT_PATH.getDirectory() + "/src/main/webapp/xslt/jats2.xsl"),
+    JATS_XSLT_PATH(PROJECT_PATH.getDirectory() + "/src/main/webapp/xslt/jats.xsl"),
     ARTICLES_PATH(PROJECT_PATH.getDirectory() + "/src/main/webapp/articles"),
     JSP_PATH("/jsp"),
     UPLOAD_FILE_PATH(UPLOADED_FILES_PATH.getDirectory() + File.separator + (new Date().getYear() + 1990) +
